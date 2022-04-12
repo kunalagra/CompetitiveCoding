93724a4354356bd7567e675fa673f717221df6df
kunalrag <goku.agra@yahoo.in>
2022-04-12 22:44:33 +0530

    Minor update to Daily Prob

--- LeetCode/289.py
+++ LeetCode/289.py
@@ -2,37 +2,34 @@ import copy
 class Solution:
 	def gameOfLife(self, board: list[list[int]]) -> None:
 		temp=copy.deepcopy(board)
+		l0=len(temp[0])
+
 		for x in range(len(temp)):
-			for y in range(len(temp[0])):
+			for y in range(l0):
 				sum1=0
-				try:
-					if x>0:
-						sum1+=temp[x-1][y]
-						if y<len(temp[0])-1:
-							sum1+=temp[x-1][y+1]
-						if y>0:
-							sum1+=temp[x-1][y-1]
-				except:
-					pass
-				try:
-					if y<len(temp[0])-1:
-						sum1+=temp[x][y+1]
+			
+				if x>0:
+					sum1+=temp[x-1][y]
+					if y<l0-1:
+						sum1+=temp[x-1][y+1]
 					if y>0:
-						sum1+=temp[x][y-1]
-				except:
-					pass
-				
-				try:
+						sum1+=temp[x-1][y-1]
+			
+				if y<l0-1:
+					sum1+=temp[x][y+1]
+				if y>0:
+					sum1+=temp[x][y-1]
+			
+				if x<len(temp)-1:
 					sum1+=temp[x+1][y]
-					if y<len(temp[0])-1:
+					if y<l0-1:
 						sum1+=temp[x+1][y+1]
 					if y>0:
 						sum1+=temp[x+1][y-1]
-				except:
-					pass
+			
 				if sum1<2:
 					board[x][y]=0
 				elif sum1==3 and temp[x][y]==0:
 					board[x][y]=1
 				elif sum1>3:
-					board[x][y]=0
+					board[x][y]=0	
